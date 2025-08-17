8c00a9c09883cbfa7bb27306ae6eb6391ee36d89
Shlok-Parihar <shlok0802@gmail.com>
2025-08-17 17:46:32 +0530

    added favicon, cart page 90% working as expected, cleaned cart and made items work, empty cart message

--- src/components/ItemsInCart/Items.jsx
+++ src/components/ItemsInCart/Items.jsx
@@ -2,7 +2,7 @@ import { List, Button, message } from "antd";
 import React, { useState, useEffect } from "react";
 import "./Items.css";
 
-const Items = ({ image, name, id, dimensions, price }) => {
+const Items = ({ image, name, id, dimensions, price, description }) => {
   // useEffect(() => {
   //     console.log("I have Arrived")
   // }, [])
@@ -11,7 +11,7 @@ const Items = ({ image, name, id, dimensions, price }) => {
 
   const handleAdd = () => {
     if (count >= 25) {
-        setCount(25)
+      setCount(25);
       message.warning("Maximum Limit Exceeded");
     } else {
       setCount((count) => count + 1);
@@ -44,8 +44,9 @@ const Items = ({ image, name, id, dimensions, price }) => {
 
           <div className="productDetails-InCart">
             <h2 style={{ fontFamily: "Geneva", marginTop: "5px" }}>{name}</h2>
-            <p>Product ID : {id}</p>
-            <p>Dimensions : {`${dimensions}`}</p>
+            <span>Product ID : {id}</span>
+            <span>Dimensions : {`${dimensions}`}</span>
+            <span className="descriptionSpan">{description}</span>
             {/* <p>Quantity : {quantity}</p> */}
             <div style={{ display: "flex", flexDirection: "row" }}>
               {/* <Button onClick={() => setCount(++count)}><h2>Remove</h2></Button>
@@ -62,14 +63,14 @@ const Items = ({ image, name, id, dimensions, price }) => {
               flexDirection: "row",
               gap: "10px",
               alignItems: "center",
-              justifyContent:"center"
+              justifyContent: "center",
             }}
           >
-            <Button onClick={handleAdd}>+</Button>
+            <Button onClick={() => handleDel()}>Remove</Button>
             <h3>
               <b>{count}</b>
             </h3>
-            <Button onClick={() => handleDel()}>-</Button>
+            <Button onClick={handleAdd}>Add</Button>
           </div>
         </div>
       </div>
--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -8,9 +8,17 @@ import data from "../../data/DummyDB.json";
 const CartPage = () => {
   const id = JSON.parse(localStorage.getItem("cart")) || [];
   const productID = data.furniture.filter((item) => id.includes(item.id));
+
   console.log("this is your id - ", id);
   console.table("this is your product id based product.", productID);
+
   const [showModal, setShowModal] = useState(false);
+  const [message, setMessage] = useState("OFFER : 10% on cart price");
+  const [shipping, setShipping] = useState(0);
+
+  useEffect(() => {
+    handleShippingCharges();
+  }, []);
 
   const handleOK = () => {
     setShowModal(false);
@@ -21,34 +29,67 @@ const CartPage = () => {
     setShowModal(true);
   };
 
-  // useEffect(() => {
-  //   if (id.length || productID.length === 0) {
-  //     return (
-  //       <>
-  //         <div>Find something you like !</div>
-  //       </>
-  //     );                                // FIX ME : says destory is not a function error.
-  //   }
-  // }, []);
+  // const total = updatedPrice + addedPrice, until each id in the id.foreach is not traversed.
 
-  const breadcrumbs = [
-    {
-      title: "Home",
-    },
-    {
-      title: "Cart",
-    },
-  ];
+  const getPrice = (id) => {
+    const product = productID.find((prod) => String(prod.id) === id);
+    const addedPrice = product ? product.price : null; //running number
+    return addedPrice;
+  };
+
+  let updatedPrice = 0;
+  id.forEach((id) => {
+    const total = updatedPrice + getPrice(id);
+    updatedPrice = total;
+    return updatedPrice;
+  });
+
+  const CalcGST = (updatedPrice) => {
+    let GST = (28 / 100) * updatedPrice;
+    return GST;
+  };
+
+  const handleShippingCharges = () => {
+    if (id.length == 0) {
+      setShipping(0);
+      return shipping;
+    } else {
+      setShipping(40);
+      return shipping;
+    }
+  };
+
+  const grandTotalPrice = updatedPrice + shipping + CalcGST(updatedPrice);
+  console.log(updatedPrice, "updateP");
+  console.log(shipping, "shipping");
+  console.log(Number(CalcGST(updatedPrice).toFixed(2), "Number CalcGST"));
+
+  const [discount, setDiscount] = useState(0);
+
+  useEffect(() => {
+    setDiscount(grandTotalPrice - grandTotalPrice * 0.1);
+  }, [grandTotalPrice]);
+
+  const PromoCode = (e) => {
+    const pCode = e.target.value.trim();
+    if (pCode.toLowerCase() === "smaran") {
+      setDiscount(grandTotalPrice - grandTotalPrice * 0.25);
+      setMessage("NEW OFFER : 25% on cart price");
+      return discount;
+    } else {
+      setDiscount(grandTotalPrice - grandTotalPrice * 0.1);
+      setMessage("OFFER : 10% on cart price");
+      return discount;
+    }
+  };
 
   return (
     <>
       <Navbar />
 
       <div className="cartMainContainer">
-        <Divider className="title">
-          <h1>Your Cart</h1>
-        </Divider>
-        <Breadcrumb className="cart-breadcrumbs" items={breadcrumbs} />
+        <h1 className="title">Your Cart</h1>
+        {/* <Breadcrumb className="cart-breadcrumbs" items={breadcrumbs} /> */}
 
         <div
           style={{
@@ -57,18 +98,37 @@ const CartPage = () => {
             justifyContent: "space-between",
           }}
         >
-          <div className="left">
-            {productID.map((i) => (
-              <Items
-                image={i.image}
-                name={i.name || "Product_Name"}
-                id={i.id}
-                price={i.price}
-                dimensions={`${i.dimensions.length} x ${i.dimensions.height} x ${i.dimensions.width}`}
-              />
-            ))}
+          {id.length === 0 ? (
+            <div className="left">
+              <div
+                style={{
+                  placeItems: "center",
+                  padding: "10px",
+                  margin: "10px",
+                }}
+              >
+                <h1>ADD SOMETHING TO YOUR CART</h1>
+                <img
+                  style={{ alignSelf: "center", display: "block" }}
+                  src="/images/emtycart.png"
+                  alt="Cart is Empty"
+                />
+              </div>
+            </div>
+          ) : (
+            <div className="left">
+              {productID.map((i) => (
+                <Items
+                  image={i.image}
+                  name={i.name || "Product_Name"}
+                  id={i.id}
+                  price={i.price}
+                  dimensions={`${i.dimensions.length} x ${i.dimensions.height} x ${i.dimensions.width}`}
+                  description={i.description}
+                />
+              ))}
 
-            <li>1st item here populate me from id</li>
+              {/* <li>1st item here populate me from id</li>
             <p>product detials list sort of a table</p>
             <p>tbale has a counter on product quantity</p>
             <p>image title and price</p>
@@ -87,32 +147,100 @@ const CartPage = () => {
               border radius to the items card and if no cards then "Add
               something to your cart.!" friendly type message wirtten in
               translucent type text.{" "}
-            </p>
-          </div>
+            </p> */}
+            </div>
+          )}
 
           <div className="right">
             <div style={{ display: "flex", flexDirection: "row", gap: "10px" }}>
               <p className="cart-font">Promo Code : </p>
               <Input
+                onPressEnter={PromoCode}
                 style={{ maxWidth: "55%", height: "45px", alignSelf: "center" }}
-              ></Input>
+              >
+                {/* <Button type="primary">Submit</Button> */}
+              </Input>
+              {/* 
+if (promoCode === "Smaran") {
+  discount = 25% // on Cart Price.
+} else {
+  discount = 10%
+} */}
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "50%",
+              }}
+            >
+              <p>Total </p> <p>{updatedPrice}</p>
             </div>
             <Divider />
-            <p>Total Amount</p>
-
-            <Divider />
-            <Divider />
-            <Button
-              type="primary"
-              color="green"
-              variant="outlined"
-              onClick={thanks4Purchase}
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "50%",
+              }}
             >
-              Checkout
-            </Button>
-
+              <p>Shipping Charges : </p> <p>{`${shipping}`}</p>
+            </div>
+            <Divider />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "50%",
+              }}
+            >
+              <p>GST : </p>
+              <p>{`${CalcGST(updatedPrice).toFixed(1) || "GST"}`}</p>
+            </div>
+            <Divider />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                width: "70%",
+              }}
+            >
+              <p>Discount: </p>
+              {message}
+              {/* <Button
+                onClick={() =>
+                  setDiscount(grandTotalPrice - grandTotalPrice * 0.1)
+                }
+              >
+                Apply
+              </Button> */}
+            </div>
+            <Divider />
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                width: "50%",
+              }}
+            >
+              <p>Grand Total : </p>
+              <p>{discount.toFixed(2)}</p>
+              {/* <p>{isNaN(discount) ? " " : discount.toFixed(2)}</p> */}
+            </div>
+            <div style={{ justifyContent: "end", display: "flex" }}>
+              <Button
+                type="primary"
+                color="green"
+                variant="outlined"
+                onClick={thanks4Purchase}
+              >
+                Checkout
+              </Button>
+            </div>
             <Modal
-              title="Thank you for your purchase"
+              title="Thank you for purchasing"
               onOk={handleOK}
               open={showModal}
               onCancel={handleOK}
